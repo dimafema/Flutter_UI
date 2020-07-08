b36344eb855051303440d0f55e8d6f92fb1401ee
Diego Fdez <dimafema@gmail.com>
2020-07-08 10:58:21 +0200

    Terminación sección 3

--- diseno_app/lib/main.dart
+++ diseno_app/lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:diseno_app/src/retos/cuadrado_animado_page.dart';
 import 'package:flutter/material.dart';
 
-import 'package:diseno_app/src/retos/cuadrado_animado_page.dart';
 
 //import 'package:diseno_app/src/pages/animaciones_pages.dart';
 //import 'package:diseno_app/src/pages/headers_page.dart';
@@ -14,6 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Diseños app',
-        home: CuadradroAnimadoPage());
+        home: CuadradoAnimadoPage());
   }
 }
--- diseno_app/lib/src/retos/cuadrado_animado_page.dart
+++ diseno_app/lib/src/retos/cuadrado_animado_page.dart
@@ -1,51 +1,78 @@
-//import 'dart:html';
-
 import 'package:flutter/material.dart';
 
-class CuadradroAnimadoPage extends StatefulWidget {
+class CuadradoAnimadoPage extends StatelessWidget {
   @override
-  _CuadradroAnimadoPageState createState() => _CuadradroAnimadoPageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _CuadradoAnimado()),
+    );
+  }
 }
 
-class _CuadradroAnimadoPageState extends State<CuadradroAnimadoPage>
+class _CuadradoAnimado extends StatefulWidget {
+  @override
+  __CuadradoAnimadoState createState() => __CuadradoAnimadoState();
+}
+
+class __CuadradoAnimadoState extends State<_CuadradoAnimado>
     with SingleTickerProviderStateMixin {
-  AnimationController contoller;
+  AnimationController controller;
   //animaciones
   Animation<double> moverDerecha;
   Animation<double> moverArriba;
   Animation<double> moverIzquierda;
   Animation<double> moverAbajo;
-
   @override
   void initState() {
+    // Inicializar
     super.initState();
-    //inicializado
-    contoller = AnimationController(
-        vsync: this, duration: Duration(microseconds: 4500));
-    moverDerecha = Tween(begin: 0.0, end: 100.0).animate(contoller);
+    controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 4500));
+    moverDerecha = Tween(begin: 0.0, end: 100.0).animate(CurvedAnimation(
+        parent: controller, curve: Interval(0, 0.25, curve: Curves.bounceOut)));
+
+    moverArriba = Tween(begin: 0.25, end: -100.0).animate(CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.25, 0.5, curve: Curves.bounceOut)));
+
+    moverIzquierda = Tween(begin: 0.0, end: -100.0).animate(CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.5, 0.75, curve: Curves.bounceOut)));
+
+    moverAbajo = Tween(begin: 0.0, end: 100.0).animate(CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.75, 1.0, curve: Curves.bounceOut)));
+
+    controller.addListener(() {
+      if (controller.status == AnimationStatus.completed) {
+        controller.reset();
+      }
+    });
   }
 
   @override
   void dispose() {
-    contoller.dispose();
+    // cerrar el controller
+    controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: CuadradoAnimado()),
-    );
-  }
-}
+    controller.forward();
 
-class CuadradoAnimado extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
     return AnimatedBuilder(
       animation: controller,
       child: _Rectangulo(),
-      builder: (BuildContext context, Widget child
+      builder: (BuildContext context, Widget child) {
+        return Transform.translate(
+            offset: Offset(
+              moverDerecha.value + moverIzquierda.value,
+              moverArriba.value + moverAbajo.value,
+            ),
+            child: child);
+      },
+    );
   }
 }
 
